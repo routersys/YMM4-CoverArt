b4e9df5ec71b466b84a6696ca604c4916a87e21b
routersys <github.routersys@gmail.com>
2026-09-26 00:29:13 +0900

    Textsの名前空間をファイル形式にする

--- CoverRenderer/Texts.cs
+++ CoverRenderer/Texts.cs
@@ -1,10 +1,8 @@
 using YukkuriMovieMaker.Generator;
 
-namespace CoverArt
-{
-    [AutoGenLocalizer]
-    partial class Texts
-    {
+namespace CoverArt;
 
-    }
+[AutoGenLocalizer]
+partial class Texts
+{
 }
